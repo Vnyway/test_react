3ebd2f3061a31906fa70b60025927a752b9a5a92
Vnywvy <ne9at1ve19@gmail.com>
2024-09-16 11:28:29 +0300

    випадне меню

--- task/src/pages/EditUsers.jsx
+++ task/src/pages/EditUsers.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import OptionsWithHeading from "../components/OptionsWithHeading";
+import { users } from "../constants";
+
 const EditUsers = () => {
+  const usernames = users.map((user) => ({ name: user.name }));
+  const [selectedUsername, setSelectedUsername] = useState(usernames[0].name);
   return (
     <div>
-      <div></div>
+      <OptionsWithHeading
+        heading="User"
+        values={usernames}
+        selected={selectedUsername}
+        setSelected={setSelectedUsername}
+      />
     </div>
   );
 };
