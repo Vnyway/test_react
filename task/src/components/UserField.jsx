e9f50037e4a251ffd81317945aff5bc7093816e9
Vnywvy <ne9at1ve19@gmail.com>
2024-09-17 09:24:52 +0300

    додано компонент UserField

---
+++ task/src/components/UserField.jsx
@@ -0,0 +1,19 @@
+import CustomButton from "./CustomButton";
+
+const UserField = ({ name, department, country, status, onClick }) => {
+  return (
+    <div className="user-field">
+      <h3>{name}</h3>
+      <span className="userfield__span">{department}</span>
+      <span>{country}</span>
+      <span>{status}</span>
+      <CustomButton
+        image="./images/trash.png"
+        onClick={() => onClick(name)}
+        disabled={false}
+      />
+    </div>
+  );
+};
+
+export default UserField;
--- task/src/pages/Users.jsx
+++ task/src/pages/Users.jsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import CustomDropdown from "../components/CustomDropdown";
 import { departments } from "../constants";
 import CustomButton from "../components/CustomButton";
+import { UserContext } from "../context";
+import UserField from "../components/UserField";
 
 const Users = () => {
+  const { newUsers, setNewUsers } = useContext(UserContext);
   const [selectedDepartments, setSelectedDepartments] = useState([]);
-
   const resetFilters = () => {
     setSelectedDepartments([]);
+  };
+
+  const handleRemoveUser = (name) => {
+    setNewUsers((pv) => pv.filter((user) => user.name !== name));
   };
 
   return (
@@ -24,6 +30,16 @@ const Users = () => {
         disabled={!selectedDepartments.length}
         onClick={resetFilters}
       />
+      {newUsers.map((user) => (
+        <UserField
+          key={user.name}
+          name={user.name}
+          department={user.department.name}
+          country={user.country.name}
+          status={user.status.name}
+          onClick={handleRemoveUser}
+        />
+      ))}
     </div>
   );
 };
