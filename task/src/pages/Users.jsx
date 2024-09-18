c7b6cf1e84b1170a9472af951c39f40fabd6c357
Vnywvy <ne9at1ve19@gmail.com>
2024-09-18 11:58:25 +0300

    фільтрування користувачів в залежності від того чи вони підпадають під критерії

--- task/src/pages/Users.jsx
+++ task/src/pages/Users.jsx
@@ -8,54 +8,80 @@ import UserField from "../components/UserField";
 const Users = () => {
   const { newUsers, setNewUsers } = useContext(UserContext);
   const [selectedDepartments, setSelectedDepartments] = useState([]);
-  const resetFilters = () => {
-    setSelectedDepartments([]);
-    setSelectedCountries([]);
-    setSelectedDepartments([]);
-  };
-
   const [selectedCountries, setSelectedCountries] = useState([]);
   const [selectedStatuses, setSelectedStatuses] = useState([]);
+
+  const resetFilters = () => {
+    setSelectedDepartments([]);
+  };
 
   const handleRemoveUser = (name) => {
     setNewUsers((pv) => pv.filter((user) => user.name !== name));
   };
 
+  const filteredUsers = newUsers.filter((user) => {
+    const departmentMatches =
+      selectedDepartments.length === 0 ||
+      selectedDepartments.some(
+        (selectedDept) => selectedDept.value === user.department.value
+      );
+
+    const countryMatches =
+      selectedCountries.length === 0 ||
+      selectedCountries.some(
+        (selectedCountry) => selectedCountry.value === user.country.value
+      );
+
+    const statusMatches =
+      selectedStatuses.length === 0 ||
+      selectedStatuses.some(
+        (selectedStatus) => selectedStatus.value === user.status.value
+      );
+
+    return departmentMatches && countryMatches && statusMatches;
+  });
+
   return (
     <main className="page-wrapper">
-      <p>Please add at least 3 departmetns to be able to proceed next steps.</p>
-      <section className="filters">
-        <div className="dropdowns-button">
-          <div className="dropdowns">
-            <CustomDropdown
-              name="Departments"
-              fields={departments}
-              selectedFields={selectedDepartments}
-              setSelectedFields={setSelectedDepartments}
-              disabled={false}
-            />
-            <CustomDropdown
-              name="Departments"
-              fields={countries}
-              selectedFields={selectedCountries}
-              setSelectedFields={setSelectedCountries}
-              disabled={selectedDepartments.length < 3}
-            />
-            <CustomDropdown
-              name="Departments"
-              fields={statuses}
-              selectedFields={selectedStatuses}
-              setSelectedFields={setSelectedStatuses}
-              disabled={selectedDepartments.length < 3}
+      <section className="sort-wrapper">
+        <p className="mistake">
+          {selectedDepartments.length < 3
+            ? "Please add at least 3 departmetns to be able to proceed next steps."
+            : ""}
+        </p>
+        <div className="filters">
+          <div className="dropdowns-button">
+            <div className="dropdowns">
+              <CustomDropdown
+                name="Departments"
+                fields={departments}
+                selectedFields={selectedDepartments}
+                setSelectedFields={setSelectedDepartments}
+                disabled={false}
+              />
+              <CustomDropdown
+                name="Departments"
+                fields={countries}
+                selectedFields={selectedCountries}
+                setSelectedFields={setSelectedCountries}
+                disabled={selectedDepartments.length < 3}
+              />
+              <CustomDropdown
+                name="Departments"
+                fields={statuses}
+                selectedFields={selectedStatuses}
+                setSelectedFields={setSelectedStatuses}
+                disabled={selectedDepartments.length < 3}
+              />
+            </div>
+            <CustomButton
+              image="/images/trash.png"
+              disabled={!selectedDepartments.length}
+              onClick={resetFilters}
             />
           </div>
-          <CustomButton
-            image="/images/trash.png"
-            disabled={!selectedDepartments.length}
-            onClick={resetFilters}
-          />
+          <CustomButton text="Add User" size="large" />
         </div>
-        <CustomButton text="Add User" size="large" />
       </section>
       <section className="table">
         <div className="headings">
@@ -65,7 +91,7 @@ const Users = () => {
           <h4>Status</h4>
           <span className="last-span"></span>
         </div>
-        {newUsers.map((user) => (
+        {filteredUsers.map((user) => (
           <UserField
             key={user.name}
             name={user.name}
